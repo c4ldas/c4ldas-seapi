3858873bebe02044a959965e44fed6aa1137295a
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-13 08:06:57 +0100

    Created callback frontpage

---
+++ app/(frontend)/callback/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect } from "react";
+
+// This page is created due to inability to change the callback URL on Streamelements oAuth (yet)
+// It just redirects to /api/callback
+export default function Callback({ _, searchParams }) {
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    window.location.assign(`${window.location.origin}/api/callback?${params}`);
+
+  }, [searchParams]);
+
+  return null;
+}
