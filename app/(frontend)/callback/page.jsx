f56022f2b6e1e7ae054920d5b8034b2029c0968c
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-14 11:07:07 +0100

    Removed console.log() and comments

--- app/(frontend)/callback/page.jsx
+++ app/(frontend)/callback/page.jsx
@@ -4,13 +4,10 @@ import { useEffect } from "react";
 // This page is created due to inability to change the callback URL on Streamelements oAuth (yet)
 // It just redirects to /api/callback
 export default function Callback({ _, searchParams }) {
-
   const params = new URLSearchParams(searchParams);
 
   useEffect(() => {
-    console.log(params);
-    /* if (params.size) */ window.location.assign(`${window.location.origin}/api/callback?${params}`);
-
+    window.location.assign(`${window.location.origin}/api/callback?${params}`);
   }, [params.size]);
 
   return null;
