93ab40ee341fd4ad9af308c5a681d4aa53e18ade
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-14 10:59:14 +0100

    Removed setInterval

--- app/(frontend)/callback/page.jsx
+++ app/(frontend)/callback/page.jsx
@@ -8,20 +8,12 @@ export default function Callback({ _, searchParams }) {
   const params = new URLSearchParams(searchParams);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      // const params = new URLSearchParams(searchParams);
+    console.log(params);
 
-      console.log(params);
+    if (params.size) {
+      window.location.assign(`${window.location.origin}/api/callback?${params}`);
+    }
 
-      if (params.size) {
-        clearInterval(interval);
-        window.location.assign(`${window.location.origin}/api/callback?${params}`);
-      }
-    }, 1000);
-
-    //const params = new URLSearchParams(searchParams);
-    //console.log("params:", params)
-    //window.location.assign(`${window.location.origin}/api/callback?${params}`);
   }, [params.size]);
 
   return null;
