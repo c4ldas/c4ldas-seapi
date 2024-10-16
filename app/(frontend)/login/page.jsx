a621445806773e37bd1e4e98579b295bd5eb2ef6
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-16 23:33:36 +0100

    Removed if conditions

--- app/(frontend)/login/page.jsx
+++ app/(frontend)/login/page.jsx
@@ -1,21 +1,16 @@
-"use server";
 import { redirect } from 'next/navigation';
 
 export default async function Login({ _, searchParams }) {
-  console.log(searchParams);
 
   const baseURL = "https://streamelements.com/oauth2/authorize?";
   const urlSearchParams = new URLSearchParams({
     response_type: "code",
     client_id: "4e2b44d1efed3fd0",
     scope: "overlays:read overlays:write",
-    redirect_uri: "https://seapi.c4ldas.com.br/api/callback",
+    redirect_uri: process.env.SE_REDIRECT_URI,
     state: searchParams.state
   });
 
-  if (searchParams.state == 'overlay_share' || searchParams.state == 'overlay_install') {
-    redirect(`${baseURL}${urlSearchParams}`)
-  }
-
-  redirect(`/`);
+  redirect(`${baseURL}${urlSearchParams}`);
 }
+
