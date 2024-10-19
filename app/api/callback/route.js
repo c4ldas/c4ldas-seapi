7846ee9f40d9912a9cb1cb6d1ab73d73a8fc6e50
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-19 12:35:06 +0100

    Removed access_token reference on cookies

--- app/api/callback/route.js
+++ app/api/callback/route.js
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { seSaveToDatabase } from "@/app/lib/database";
-import { getTokenCode, getUserData, decodeData } from "@/app/lib/streamelements";
+import { getTokenCode, getUserData, decodeData, generateTag } from "@/app/lib/streamelements";
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
@@ -9,14 +9,15 @@ export async function GET(request) {
   const origin = request.nextUrl.origin;
 
   if (obj.error) return Response.redirect(`${origin}/?error=${obj.error}`);
-
   if (state.status == "failed" || (!state.startsWith("overlay_share") && !state.startsWith("overlay_install"))) return Response.redirect(`${origin}?error=State changed during login. Please try again.`);
 
   const token = await getTokenCode(obj.code);
   const user = await getUserData(token.access_token);
+  const tag = await generateTag();
 
   const data = {
-    id: user._id,
+    tag: tag,
+    account_id: user._id,
     username: user.username,
     access_token: token.access_token,
     refresh_token: token.refresh_token,
@@ -25,9 +26,9 @@ export async function GET(request) {
   const saved = await seSaveToDatabase(data);
   if (!saved) return Response.redirect(`${origin}?error=Error while saving to database. Please try again later.`);
 
-  cookies().set('se_id', data.id);
+  cookies().set('se_id', data.account_id);
   cookies().set('se_username', data.username);
-  cookies().set('se_access_token', data.access_token);
+  cookies().set('se_tag', data.tag);
 
   return Response.redirect(`${origin}/${state.split("_")[1]}`);
 }
