a5af6a07faaefdfe8c294b59deb3ad617b2b7ad2
Rodrigo Caldas <c4ldas@gmail.com>
2024-10-22 04:41:53 +0100

    Added header-links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import DarkMode from "./DarkMode";
 
-export default function Header() {
+export default function Header(props) {
 
   return (
     <header className="header">
@@ -20,6 +20,15 @@ export default function Header() {
           placeholder="empty"
         />
       </Link>
+      <div className="header-links">
+        <Link id="home" href="/"><div style={{ color: "var(--color)" }}>Home</div></Link>
+        <Link id="share" href="/share"><div style={{ color: "var(--color)" }}>Share overlay</div></Link>
+        <Link id="install" href="/install"><div style={{ color: "var(--color)" }}>Install overlay</div></Link>
+        <Link id="show-shared" href="/show-shared"><div style={{ color: "var(--color)" }}>Overlays shared</div></Link>
+        <Link id="leaderboard" href="/leaderboard"><div style={{ color: "var(--color)" }}>Leaderboard</div></Link>
+        <Link id="endpoints" href="/endpoints"><div style={{ color: "var(--color)" }}>Endpoints</div></Link>
+      </div>
+
       <DarkMode />
     </header>
   )
