ebfa72bc733ce13414b2b516b0695be635d6482c
Rodrigo Caldas <c4ldas@gmail.com>
2024-11-21 06:39:25 +0000

    Changed ID

--- app/(frontend)/login/page.jsx
+++ app/(frontend)/login/page.jsx
@@ -5,7 +5,7 @@ export default async function Login({ _, searchParams }) {
   const baseURL = "https://streamelements.com/oauth2/authorize?";
   const urlSearchParams = new URLSearchParams({
     response_type: "code",
-    client_id: "4e2b44d1efed3fd0",
+    client_id: process.env.SE_CLIENT_ID,
     scope: "overlays:read overlays:write",
     redirect_uri: process.env.SE_REDIRECT_URI,
     state: searchParams.state
