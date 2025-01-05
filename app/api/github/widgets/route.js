84faac94de2316418ea3428e87280c6230596a72
Rodrigo Caldas <c4ldas@gmail.com>
2025-01-05 16:01:00 +0000

    Added 10 min revalidation

--- app/api/github/widgets/route.js
+++ app/api/github/widgets/route.js
@@ -17,6 +17,7 @@ export async function getGithubWidgets() {
   try {
     // Get the directory list from the repository
     const request = await fetch('https://api.github.com/repos/c4ldas/streamelements-widgets/contents/', {
+      "next": { revalidate: 600 }, // 10 minutes
       "headers": {
         "Accept": "application/vnd.github.v3.raw",
         "Authorization": `Bearer ${process.env.GITHUB_API_KEY}`
@@ -32,6 +33,7 @@ export async function getGithubWidgets() {
 
       // Get the README of current directory as HTML format
       const htmlRequest = await fetch(`https://api.github.com/repos/c4ldas/streamelements-widgets/readme/${item.name}`, {
+        "next": { revalidate: 600 }, // 10 minutes
         "headers": {
           "accept": "application/vnd.github.v3.raw",
           "Authorization": `Bearer ${process.env.GITHUB_API_KEY}`
