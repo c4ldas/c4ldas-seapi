06e2ca5a2f861513974b477ffae209b1d830a6a3
c4ldas <c4ldas@gmail.com>
2025-12-23 14:23:13 +0000

    Create new metadata title and description

---
+++ app/(frontend)/overlays/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Overlay Share",
+  description: "Share your overlays with your friends.",
+};
+
+export default function Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
