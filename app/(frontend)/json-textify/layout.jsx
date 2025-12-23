b5098eefe3fd0fdb3a6c406b7047762116b9fc99
c4ldas <c4ldas@gmail.com>
2025-12-23 14:23:24 +0000

    Create new metadata title and description

---
+++ app/(frontend)/json-textify/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "JSON Textify",
+  description: "Generate plain text response from JSON output"
+};
+
+export default function Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
