af468b86b927e9c916b9e21b80a30bd82f556227
c4ldas <c4ldas@gmail.com>
2025-12-23 14:23:18 +0000

    Create new metadata title and description

---
+++ app/(frontend)/chat-commands/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Chat Commands Transfer",
+  description: "Copy chat commands from one channel to another.",
+};
+
+export default function Layout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
