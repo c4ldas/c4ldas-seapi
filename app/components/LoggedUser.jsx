7f2dc3d3a0910308abd91d82349eb320a0508f78
c4ldas <c4ldas@gmail.com>
2025-12-29 00:58:16 +0000

    Create LoggedUser component

---
+++ app/components/LoggedUser.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function LoggedUser(props, asdf) {
+  console.log("props:", props);
+  console.log("asdf:", asdf);
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "10%" }}>
+      <div>
+        <p><strong>Channel name:</strong> {props.username} </p>
+        <p><strong>Channel ID:</strong> {props.id}</p>
+        <p><strong>Platform:</strong> {props.provider}</p>
+      </div>
+      <img src={props.avatar} alt="avatar" style={{ borderRadius: "50%", width: "100px", height: "100px" }} />
+    </div>
+  )
+}
